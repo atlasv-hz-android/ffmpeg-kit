b8ac516c3a0848e7a1d4f0a2de2dc356bab3f1f0
weiping <weiping@atlasv.com>
2025-08-15 16:00:22 +0800

    build:构建全部的CPU架构

--- build_android.sh
+++ build_android.sh
@@ -2,4 +2,4 @@ export ANDROID_SDK_ROOT=/Users/mwp/Library/Android/sdk
 export ANDROID_NDK_ROOT=/Users/mwp/Library/Android/sdk/ndk/28.2.13676358
 ./android.sh \
 --enable-gpl --enable-libvidstab --enable-rubberband --enable-x264 --enable-x265 --enable-xvidcore \
---disable-arm-v7a --disable-arm-v7a-neon --disable-x86 --disable-x86-64 
+# --disable-arm-v7a --disable-arm-v7a-neon --disable-x86 --disable-x86-64
